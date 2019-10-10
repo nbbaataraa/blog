a6cca829039c12e5d4af0fbc0f8ef3d7d27af5e2
nbbaataraa <satanhero.nb@gmail.com>
2019-10-10 16:21:50 +0800

    Changes

--- resources/views/layouts/sidebarf.blade.php
+++ resources/views/layouts/sidebarf.blade.php
@@ -1,8 +1,8 @@
 <div class="sidebar" data-color="orange" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
 
   <div class="logo">
-    <a href="#" class="simple-text logo-normal">
-      ЭТТ Хүний нѳѳц
+    <a  class="simple-text logo-normal">
+      "ЭТТ" ХК Салбар
     </a>
   </div>
   <div class="sidebar-wrapper">
@@ -16,9 +16,29 @@
 
       <li class="nav-item active  ">
         <a class="nav-link" href="{{url('/home')}}">
-          <i class="material-icons">library_books</i>
-
+          <i class="material-icons">supervised_user_circle</i>
           <p>Хүний нөөц</p>
+        </a>
+      </li>
+
+      <li class="nav-item active  ">
+        <a class="nav-link" href="{{url('/home')}}">
+          <i class="material-icons">library_books</i>
+          <p>Гэрээ</p>
+        </a>
+      </li>
+
+      <li class="nav-item active  ">
+        <a class="nav-link" href="{{url('/home')}}">
+          <i class="material-icons">archive</i>
+          <p>Архив</p>
+        </a>
+      </li>
+
+      <li class="nav-item active  ">
+        <a class="nav-link" href="{{url('/home')}}">
+          <i class="material-icons">tv</i>
+          <p>Диспатчер</p>
         </a>
       </li>
 
